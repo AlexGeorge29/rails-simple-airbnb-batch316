65f212f9d2bb3e241339599d3882ef2552d41218
AlexandreGeorge <alexandreantoinegeorge@gmail.com>
2020-01-27 17:52:13 -0500

    model and validation

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,6 @@
 class Flat < ApplicationRecord
+  GUESTS = Array(1..10)
+
+  validates :name, :address, :description, :price_per_night, presence: true
+  validates :number_of_guests, inclusion: { in: GUESTS }
 end
